9c833154d8b6830bb0dd64b43b4a3e3bd513edfc
TaurineMerge <plotnikov12341@gmail.com>
2025-11-23 22:31:16 +0300

    chore: add drizzle configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
+
+export default {
+  dialect: 'postgresql',
+  schema: './src/infrastructure/database/schema/*',
+  out: './src/infrastructure/database/migrations',
+  driver: 'pglite',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
